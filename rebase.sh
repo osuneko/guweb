c9020c42a83ebb20305ad95eb0de293856a8b971
minisbett <no>
2022-05-02 15:26:52 +0200

    idkkk

--- rebase.sh
+++ rebase.sh
@@ -1,12 +1,6 @@
 git remote rm origin
 git remote add origin https://github.com/Varkaria/guweb
 
-<<<<<<< HEAD
 git pull --rebase
 
 ./rebase2.sh
-=======
-git pull origin main --rebase
-
-./rebase2.sh
->>>>>>> Added automatic rebase2.sh call to rebase.sh
