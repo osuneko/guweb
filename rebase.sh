cc3aab4fc9d0bd4b9fe8196a9813b0aa9c9520c3
minisbett <no>
2022-05-02 19:40:56 +0200

    ??????????

--- rebase.sh
+++ rebase.sh
@@ -1,6 +1,18 @@
 git remote rm origin
 git remote add origin https://github.com/Varkaria/guweb
 
+<<<<<<< HEAD
 git pull origin main --rebase
 
 ./rebase2.sh
+=======
+<<<<<<< HEAD
+git pull --rebase
+
+./rebase2.sh
+=======
+git pull origin main --rebase
+
+./rebase2.sh
+>>>>>>> Added automatic rebase2.sh call to rebase.sh
+>>>>>>> Added automatic rebase2.sh call to rebase.sh
--- rebase2.sh
+++ rebase2.sh
@@ -1,8 +1,4 @@
 
 
 git remote rm origin
-<<<<<<< HEAD
 git remote add origin https://github.com/cs0su/guweb
-=======
-git remote add origin https://github.com/cs0su/guweb
->>>>>>> Added topg and oauth locations, split rebase script
