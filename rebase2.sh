9cb06c07bc55d58aecbcb126f8dfda095c589c0f
minisbett <no>
2022-05-02 14:50:51 +0200

    Added topg and oauth locations, split rebase script

--- rebase2.sh
+++ rebase2.sh
@@ -1,4 +1,8 @@
 
 
 git remote rm origin
+<<<<<<< HEAD
 git remote add origin https://github.com/cs0su/guweb
+=======
+git remote add origin https://github.com/cs0su/guweb
+>>>>>>> Added topg and oauth locations, split rebase script
