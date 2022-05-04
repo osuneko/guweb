f96360d38ad35e45922be231effa6d7cb0449a37
minisbett <no>
2022-05-04 10:13:36 +0200

    Removed rebase scripts

--- rebase.sh
+++
@@ -1,6 +0,0 @@
-git remote rm origin
-git remote add origin https://github.com/Varkaria/guweb
-
-git pull origin main --rebase
-
-./rebase2.sh
--- rebase2.sh
+++
@@ -1,2 +0,0 @@
-git remote rm origin
-git remote add origin https://github.com/cs0su/guweb
